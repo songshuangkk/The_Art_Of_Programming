6c9672b172effde04854388b43af1ec751fefa68
宋 <songshuangkk@gmail.com>
2014-03-17 14:41:28 +0800

    Update djjikst.c

--- djjikst.c
+++ djjikst.c
@@ -14,17 +14,16 @@ char path[20] = {0};
 int start_flag = 0;
 int end_flag = 0;
 
-int path_num = 0;
-
 Node grap[7] = {{'A',{0,2,6,8,1000,1000,1000}},{'B',{2,0,3,1000,1000,1000,1000}},
 {'C',{6,3,0,9,11,1000,1000}},{'D',{8,1000,9,0,13,17,1000}},{'E',{1000,1000,11,13,0,6,1000}},
 {'F',{1000,1000,1000,17,13,0,1000}},{'G',{1000,1000,1000,1000,1000,1000,0}}};
 
 void search(char start, char end)
 {
-	int i,j,t;
-	int n = 1;
+	int i,j;
+	int n = 0;
 	int flag = 0;
+	int m = 0;
 	int use[7] = {0};
 
 	for(i=0; i<7; i++)
@@ -37,12 +36,10 @@ void search(char start, char end)
 
 	use[start_flag] = 1;
 
-	path[0] = grap[start_flag].name;
-	path_num++;
-
 	while(n<7)
 	{
 		int tempmin = MAX;
+		
 		for(i=0; i<7; i++)
 		{
 			if(use[i]==0&&grap[start_flag].length[i]<tempmin&&i!=start_flag)
@@ -53,26 +50,49 @@ void search(char start, char end)
 		}
 		use[flag] = 1;
 		n++;
-		t = flag;
+
 		/*Update the length*/
 		for(j=0; j<7;j++)
 		{
 			if(grap[flag].length[j]+tempmin<grap[start_flag].length[j]&&use[j]==0&&grap[flag].length[j]!=MAX)
 			{
 				grap[start_flag].length[j] = grap[flag].length[j]+tempmin;
-				t = j;
 			}
 		}
 
+		
 		if(flag == end_flag)
 		{
-			printf("path:%d\n", grap[start_flag].length[end_flag]);
+			printf("%d\n", grap[start_flag].length[end_flag]);
+			int min = MAX;
+			while(n>0)
+			{
+				path[m++] = grap[flag].name;
+				for(i=0;i<n;i++)
+				{
+					if(use[i]==1&&grap[flag].length[i]<min&&i!=flag)
+					{
+						min = grap[flag].length[i];
+						j = i;
+					}
+				}
+				flag = j;
+				use[i] = 0;
+				if(grap[flag].name == grap[start_flag].name){
+					path[m++] = grap[flag].name;
+					break;
+				}
+				n--;
+			}
 			break;
 		}
 	}
-	
 
-
+	for(i=m-1; i>=0; i--)
+	{
+		printf("%c->", path[i]);
+	}
+	printf("\n");
 }
 
 int
