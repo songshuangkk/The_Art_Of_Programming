5b39313dc01189fa5287b47b4cbbe482e72efaed
宋 <songshuangkk@gmail.com>
2014-03-21 16:35:07 +0800

    Create create.py

---
+++ create.py
@@ -0,0 +1,30 @@
+#!D:\Python\python.exe
+#encoding=utf-8
+import re
+import os
+import sys
+import ConfigParser
+
+f_name  = file('./create.txt', 'r')
+f_check = file('./return.txt', 'w')
+
+name = f_name.readline()
+passwd = f_name.readline()
+
+name = name[:-1]
+passwd = passwd[:-1]
+
+"""Create the user password"""
+cmd = "htpasswd -D d:\Apache\conf\users "
+cmd = cmd + name + '' + passwd
+ret = os.system(cmd)
+
+if ret == 0:
+    c = '1'
+    f_check.write(c)
+else:
+    c = '0'
+    f_check.write(c)
+        
+f_check.close()
+f_name.close()
